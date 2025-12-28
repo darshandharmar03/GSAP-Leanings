ecb2fb47354eac092438ac04460fe465272a15ec
darshandharmar03 <darshandharmar03@gmail.com>
2025-12-28 14:45:26 +0530

    updates changes

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ gsap.to("#box1", {
  });
  gsap.from("#box2", {     // From method it will take the animation final to initial
     opacity:1 ,// Make the box disappear
-    x: 1000,     // Move 100 pixels to the right
-    y: 50 ,    // Move 200 pixels down
-    duration: 2,   // Animation duration in seconds
-    delay: 1,    // Delay before the animation starts
-    rotate:360,  // Rotate 360 degrees
-    backgroundColor: "blue", // Change background color to blue (must be double quotes and backgroundColor must be camel case)
-    repeat:2,      // Repeat the animation 2 times  + original play
+    x: 1000,
+    y: 50 ,   
+    duration: 2,  
+    delay: 1,  
+    rotate:360,  
+    backgroundColor: "blue",
     repeat:-1,     // Infinite Repeats
     yoyo:true   // yo yo will reverse the animation on each repeat
  });
@@ -28,7 +27,7 @@ gsap.to("#navbar h2",{
     stagger:1,   // Stagger will apply delay between each element
     
 })
-let tl = gsap.timeline() // Timeline will hep to chain multiple animaitons you will observe after the animation of box3 remaining animations will start performing
+let tl = gsap.timeline() //  Timeline will hep to chain multiple animaitons you will observe after the animation of box3 remaining animations will start performing
 tl.to("#box3",{
     opacity:1,
     x:300,
